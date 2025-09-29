0b3e6b8d350029d337250346eec1929db7db8332
guilhermesquita <guilhermer_mesquita@hotmail.com>
2025-09-29 13:24:59 -0400

    fix: remove code not used

--- backend/src/seeds/runSeeds.ts
+++ backend/src/seeds/runSeeds.ts
@@ -1,8 +1,3 @@
 import { seedWinners } from "./seedWinners";
 
-// export function runSeeds() {
-//   seedWinners();
-// }
-
-// runSeeds();
 seedWinners();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import app from "./app";
 import { connectDB } from "./db";
-// import { runSeeds } from "./seeds/runSeeds";
 
 const PORT = process.env.PORT || 3000;
 
 async function start() {
   await connectDB();
-  // runSeeds();
 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
